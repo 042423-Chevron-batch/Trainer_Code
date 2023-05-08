0bee6fd9820df16684653ae1ae8d68d3cd5607b5
Mark Moore <mark.moore@revature.com>
2023-05-08 14:59:08 -0500

    ApplAndOrange challenge

---
+++ HackerRankChallenges/AppleAndOrange.cs
@@ -0,0 +1,5 @@
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+    {
+//lets pretend I solved this....
+//// and this is my verbose commentary
+    }

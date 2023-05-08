4179d5ac302d92f6f209efa36c9c2884fa0a31b7
Mark Moore <mark.moore@revature.com>
2023-05-08 11:49:21 -0500

    adds git ppt and a text file to demo merge conflicts

---
+++ W3/SinglyLinkedLisk/Program.cs
@@ -0,0 +1,26 @@
+﻿using SingleyLinkedListNamespace;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // add some nodes
+        SingleyLinkedList sll = new SingleyLinkedList();
+
+        sll.AddToFront("1Mark", "1Moore");
+        sll.AddToFront("2Mark", "2Moore");
+        sll.AddToFront("3Mark", "3Moore");
+        sll.AddToFront("4Mark", "4Moore");
+
+        List<Person> lp = sll.GetPeopleInOrder();
+
+        foreach (Person p in lp)
+        {
+            Console.WriteLine($"{p.Fname} {p.Lname}");
+        }
+
+
+    }
+}

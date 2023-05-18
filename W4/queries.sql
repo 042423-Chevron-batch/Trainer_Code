bda5ed1b710b44287afc570fc67748fb82d142ec
Mark Moore <mark.moore@revature.com>
2023-05-18 12:26:01 -0500

    adds some drawrings and stored procedures and SQL functions

--- W4/queries.sql
+++ W4/queries.sql
@@ -1,4 +1,3 @@
-SELECT TOP (1000) * FROM [dbo].[Customer]
 
 -- #5 challenge
 SELECT FirstName, Address FROM Customer WHERE Country = 'Brazil' ORDER BY FirstName DESC;
@@ -22,7 +21,7 @@ GROUP BY A.Name
 HAVING COUNT(T.TrackId) > 1
 ORDER BY Name;
 
--- show all Customers starting with the highest id number
+-- show top 1000 Customers starting with the highest id number
 SELECT TOP (1000) * FROM [dbo].[Customer] Order by CustomerId desc;
 
 --create a subquery to get the number of items in the table.
@@ -32,3 +31,77 @@ INSERT INTO Customer (CustomerId, FirstName, LastName,Email) VALUES ((SELECT MAX
 SELECT CustomerId, FirstName, LastName, Email FROM [dbo].[Customer] WHERE FirstName = 'James Alan' AND LastName = 'Moore'; 
 
 SELECT CustomerId, FirstName, LastName, Email FROM [dbo].[Customer];
+
+-- get the average of 3 decimal args
+-- (Scalar) Functions return a single value
+CREATE FUNCTION GetMyAverage(@grade1 decimal(5,2), @grade2 decimal(5,2), @grade3 decimal(5,2))
+RETURNS decimal(5,2)
+AS
+BEGIN
+	DECLARE @avg decimal(5,2);	
+	SELECT @avg = (@grade1 +@grade2+@grade3)/3;
+	RETURN @avg;
+END
+
+SELECT dbo.GetMyAverage(100.00, 12.32, 47.62) AS AveragedValue;
+
+-- CHINOOK - Table-Value Function - Get a selected number of artists and their numebr of tracks.
+-- TVFs return a whole table as the result
+CREATE FUNCTION GetNumOfArtistsAndTheirNumTracks(@tops INT)
+RETURNS TABLE
+AS
+RETURN
+	(SELECT TOP (@tops) A.Name AS Name, COUNT(T.TrackId) AS NumSongs 
+	FROM [dbo].[Artist] A 
+	Left Join [dbo].[Album] Alb ON A.ArtistId = Alb.ArtistId
+	Left Join [dbo].[Track] T ON Alb.AlbumId = T.AlbumId
+	GROUP BY A.Name 
+	HAVING COUNT(T.TrackId) > 1
+	ORDER BY Name);
+
+SELECT * FROM GetNumOfArtistsAndTheirNumTracks(10);
+
+
+-- TVFs return all the distinct support rep ids
+CREATE FUNCTION GetDistinctSupportReps()
+RETURNS TABLE
+AS
+RETURN
+	(SELECT TOP(100) C.SupportRepId As ID, E.FirstName AS Fname, E.LastName AS Lname 
+	FROM [dbo].[Customer] C
+	LEFT JOIN [dbo].[Employee] E ON C.SupportRepId = E.EmployeeId
+	WHERE C.SupportRepId > 0
+	ORDER BY C.SupportRepId);
+
+SELECT * FROM GetDistinctSupportReps();
+DROP FUNCTION GetDistinctSupportReps
+
+
+--Stored Prodecure - Get a number of support reps and their names
+CREATE PROCEDURE GetSupportReps(@howMany INT)
+AS
+	SELECT TOP(@howMany) C.SupportRepId As ID, E.FirstName AS Fname, E.LastName AS Lname 
+	FROM [dbo].[Customer] C
+	LEFT JOIN [dbo].[Employee] E ON C.SupportRepId = E.EmployeeId
+	WHERE C.SupportRepId > 0
+	ORDER BY C.SupportRepId;
+
+EXEC GetSupportReps 10;
+DROP Procedure GetSupportReps
+
+-- CHINOOK - Create a stored procedure that gets the Employees with a substring of the param string in their title.
+CREATE PROCEDURE GetEmployeesWithTitleSubString(@contains NVARCHAR(10), @howmany INT OUTPUT)
+AS
+	SELECT FirstName AS Fname, LastName AS Lname, Title
+	FROM [dbo].[Employee]
+	WHERE Title LIKE CONCAT('%',@contains,'%');
+  --you can have multiple SELECT statements in a procedure.
+	SELECT @howmany = COUNT(*) FROM [dbo].[Employee]
+	WHERE Title LIKE CONCAT('%',@contains,'%');
+
+-- run these three lines together so that @res is created to be used in the EXEC and SELECT statements below it.
+DECLARE @res INT;
+EXEC GetEmployeesWithTitleSubString 'Sales', @res OUTPUT;
+Select @res AS HowManyRows;
+
+DROP Procedure GetEmployeesWithTitleSubString;

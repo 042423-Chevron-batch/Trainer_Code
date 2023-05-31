b0dfadbd48bc0d4f7f3f24913078dc3660397e42
Minseon Song <songminseonk@gmail.com>
2023-05-31 10:31:49 -0400

    w6 js

---
+++ W6/index.js
@@ -0,0 +1,19 @@
+function btnClick(e) {
+    console.log("Button was clicked!", e);
+
+    
+}
+
+if(true) {
+    let cat = 'aurynie';
+    var dog = 'ellie & pancake';
+}
+
+// console.log(cat); //reference error
+console.log(dog);
+let arr = [1, 2, 3, 4, 5];
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log(i);
